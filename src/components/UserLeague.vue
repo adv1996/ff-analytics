5cb5f82f9de61c82e559c4dc2bf4c72f9ecedf83
Advaith Venkatakrishnan <advaith96@gmail.com>
2020-01-12 23:18:37 -0500

    added lodash, added sorting to table

--- src/components/UserLeague.vue
+++ src/components/UserLeague.vue
@@ -5,17 +5,25 @@
     <table class="table-auto">
       <thead>
         <tr>
-          <th class="px-4 py-2 font-normal">User</th>
-          <th class="px-4 py-2 font-normal">League</th>
-          <th class="px-4 py-2 font-normal">Roster</th>
-          <th class="px-4 py-2 font-normal">Wins</th>
-          <th class="px-4 py-2 font-normal">Losses</th>
-          <th class="px-4 py-2 font-normal">Points For</th>
-          <th class="px-4 py-2 font-normal">Points Against</th>
+          <th class="px-4 py-2 font-normal" @click="sort('user_id')">User</th>
+          <th class="px-4 py-2 font-normal" @click="sort('league_id')">
+            League
+          </th>
+          <th class="px-4 py-2 font-normal" @click="sort('roster_id')">
+            Roster
+          </th>
+          <th class="px-4 py-2 font-normal" @click="sort('wins')">Wins</th>
+          <th class="px-4 py-2 font-normal" @click="sort('losses')">Losses</th>
+          <th class="px-4 py-2 font-normal" @click="sort('fpts')">
+            Points For
+          </th>
+          <th class="px-4 py-2 font-normal" @click="sort('fpts_against')">
+            Points Against
+          </th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="userLeague in userLeagues" :key="userLeague.id">
+        <tr v-for="userLeague in sortedUserLeague" :key="userLeague">
           <td class="border px-4 py-2">{{ userLeague.user_id }}</td>
           <td class="border px-4 py-2 font-hairline">
             {{ userLeague.league_id }}
@@ -33,11 +41,21 @@
 
 <script>
 import gql from "graphql-tag";
+import _ from "lodash";
+
 export default {
   name: "UserLeague",
+  computed: {
+    sortedUserLeague() {
+      let sortedData = _.cloneDeep(this.userLeagues);
+      return _.orderBy(sortedData, [this.currentColumn], [this.orderType]);
+    }
+  },
   data() {
     return {
-      userLeagues: []
+      userLeagues: [],
+      currentColumn: "user_id",
+      orderType: "desc"
     };
   },
   apollo: {
@@ -57,6 +75,19 @@ export default {
       `,
       result({ data }) {
         this.userLeagues = data.UserLeague;
+      }
+    }
+  },
+  methods: {
+    sort(column) {
+      if (column !== this.currentColumn) {
+        this.currentColumn = column;
+      } else {
+        if (this.orderType === "desc") {
+          this.orderType = "asc";
+        } else {
+          this.orderType = "desc";
+        }
       }
     }
   }
